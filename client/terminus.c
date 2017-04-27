f87a9542f72748d1418a5ef1549570a1e0bbc59a
saalik <saalik@Terminus.localdomain>
2017-04-27 14:55:42 +0200

    init

---
+++ client/terminus.c
@@ -0,0 +1,22 @@
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+/* Include for later .h empty for now */
+#include <terminus.h>
+
+
+int main(int argc, char ** argv)
+{
+	int fd, i;
+	int nb_args, *arg;
+	
+	
+
+
+}
