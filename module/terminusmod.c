f60aa7a3a14291d4279c2d242dcbe2ed2fdd3d04
saalik <saalik@Terminus.localdomain>
2017-04-27 17:05:23 +0200

    Added some stuff

--- module/terminusmod.c
+++ module/terminusmod.c
@@ -9,24 +9,29 @@
 #include <linux/mm.h>
 
 /* Still useless for now */
-#include "../inc/kercli.h"
+#include "../inc/terminux.h"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Oskar Viljasaar, Saalik Hatia");
 MODULE_DESCRIPTION("PNL Project UPMC - Terminus");
 
-long unioctl (struct file *filp,
-			unsigned int cmd, 
-			unsigned long arg);
+int dev_num;
 
+long iohandler (struct file *filp,
+	      unsigned int cmd, 
+	      unsigned long arg);
 
 static const struct file_operations fops = {
 	.unlocked_ioctl = unioctl,
 };
 
+static struct workqueue_struct workst;
 
 DECLARE_WAIT_QUEUE_HEAD(cond_wait_queue);
 static bool cond;
+
+
+
 
 static int __init init (void)
 {
