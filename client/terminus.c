28dc887320cfeca2503c7251e50c2b2e8f1f058c
Oskar Viljasaar <oskar.viljasaar@eesti.ee>
2017-05-04 16:36:31 +0200

    lelele lsmod clietn

--- client/terminus.c
+++ client/terminus.c
@@ -10,9 +10,28 @@
 /* Include for later .h empty for now */
 #include <terminus.h>
 
+#define T_PATH "/tmp/terminus"
 
 int main(int argc, char ** argv)
 {
+	int fd = 0;
+
+	char *ptr = NULL;
+
+	fd = open(PATH, O_RDWR);
+
+	if (fd == -1) {
+		print("no fd");
+		exit(1);
+	}
+
+	ptr = (int *) malloc(T_BUF_SIZE * sizeof(char));
+
+	if (ptr == NULL) {
+		printf("no malloc");
+		exit(1);
+	}
+
 	if (argc <= 1) {
 		printf("usage wesh\n");
 		return 1;
@@ -20,7 +39,12 @@ int main(int argc, char ** argv)
 
 	if (strcmp(argv[1], "lsmod") == 0) {
 		printf("lsmod\n");
+		if (ioctl(fd, T_LSMOD, ptr) == 0) {
+			printf("%s", ptr);
+		}
 	}
+
+	close(fd);
 
 
 
