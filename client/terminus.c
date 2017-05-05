a3c8c21d0f391a934ba46279688a2fd905f9d9d8
Oskar Viljasaar <oskar.viljasaar@gmail.com>
2017-05-05 16:21:33 +0200

    sdkjn

--- client/terminus.c
+++ client/terminus.c
@@ -67,7 +67,7 @@ void modinfo(int fd, char* module_name)
 	tmp_ptr = info_module.arg;
 	printf("test\n");
 	while (tmp_ptr != NULL) {
-		printf("%c", tmp_ptr);
+		printf("%c", *tmp_ptr);
 		fflush(stdout);
 		if (*tmp_ptr == '\n')
 			*tmp_ptr = '\0';
