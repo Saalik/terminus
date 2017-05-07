f4c65ff4797cd563964e88a0548553476c9f3f65
saalik <saalik@Terminus.localdomain>
2017-05-07 22:40:18 +0200

    4

--- module/terminusmod.c
+++ module/terminusmod.c
@@ -177,6 +177,7 @@ long iohandler(struct file *filp, unsigned int cmd, unsigned long arg)
 	struct modinfo_waiter mow;
 	
 	switch(cmd) {
+
 	case T_LIST:
 		break;
 	case T_FG:
@@ -201,6 +202,8 @@ long iohandler(struct file *filp, unsigned int cmd, unsigned long arg)
 		mow = kzalloc(sizeof(struct modinfo_waiter), GFP_KERNEL);
 		mow->async=0;
 		INIT_WORK(&(mow->ws), t_modinfo);
+		schedule_work(&(mow->ws), t_modinfo);
+		wait_event(cond_wait_queue, sleep!=0);
 		copy_to_user((void*)arg, &mew->im, sizeof(struct infomod));
 		kfree(mow);
 		break;
@@ -244,6 +247,35 @@ static void t_meminfo(struct work_struct *work)
 
 static void t_modinfo(struct work_struct *work)
 {
+	struct module *mod;
+	struct infomod im;
+	union arg_infomod info_mod;
+	struct modinfo_waiter mow;
+	char *mod_name = NULL;
+	int i = 0;
 
+	copy_from_user(&info_mod, arg, sizeof(char) * T_BUF_STR);
+	mod_name = info_mod.arg;
+	pr_info("module name %s\n", mod_name);
+	mod = find_module(mod_name);
+
+	if (mod != NULL) {
+		scnprintf(im.name, T_BUF_STR, "%s", mod->name);
+		scnprintf(im.version, T_BUF_STR, "%s", mod->version);
+		im.module_core = mod->module_core;
+		im.num_kp = mod->num_kp;
+		while (i < mod->num_kp) {
+			/*kernel paramkp */
+			scnprintf(im.args, T_BUF_STR, "%s ", mod->kp[i].name);
+			i++;
+		}
+
+	} else {
+		im.module_core = NULL;
+	}
+	mow->im=im;
+	
+	sleep=1;
+	wake_up(&cond_wait_queue);
 }
 
